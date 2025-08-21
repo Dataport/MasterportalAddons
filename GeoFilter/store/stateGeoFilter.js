177a9ad2faee176a983dcb7c07eccc6a7761b825
bruehlca <carina.bruehl@dataport.de>
2025-08-21 09:42:32 +0200

    add new states

--- GeoFilter/store/stateGeoFilter.js
+++ GeoFilter/store/stateGeoFilter.js
@@ -1,7 +1,9 @@
 const state = {
     icon: "bi-filter",
     id: "geoFilter",
-    name: "additional:modules.tools.geoFilter.title"
+    type: "geoFilter",
+    name: "additional:modules.tools.geoFilter.title",
+    description: "Hier kann was gemacht werden"
 };
 
 export default state;
