0eada9a58488fea5e30b7836db1c96ba00ee066d
bruehlca <carina.bruehl@dataport.de>
2025-08-22 12:34:09 +0200

    fetch intersecting features from server

--- GeoFilter/components/GeoFilter.vue
+++ GeoFilter/components/GeoFilter.vue
@@ -3,6 +3,9 @@ import {mapGetters} from "vuex";
 import layerCollection from "@core/layers/js/layerCollection";
 import FlatButton from "@shared/modules/buttons/components/FlatButton.vue";
 import SpinnerItem from "@shared/modules/spinner/components/SpinnerItem.vue";
+import WFS from "ol/format/WFS";
+import {intersects, or} from "ol/format/filter";
+import Polygon from "ol/geom/Polygon";
 
 /**
  * Checks if the given layer is a polygon layer.
@@ -29,13 +32,28 @@ export default {
     computed: {
         ...mapGetters("Modules/GeoFilter", ["filterLayerTypes", "targetLayerIds"]),
         filterLayers () {
-            return layerCollection.getLayers().filter(layer => this.filterLayerTypes.includes(layer.layer.get("typ")) && isPolygonLayer(layer));
+            return layerCollection.getLayers().filter(layer => this.filterLayerTypes.includes(layer.layer.get("typ")) && isPolygonLayer(layer)).map(layer => {
+                return {
+                    id: layer.layer.get("id"),
+                    name: layer.layer.get("name")
+                };
+            });
         },
         targetLayers () {
+            let targetLayers = [];
+
             if (this.targetLayerIds.length > 0) {
-                return layerCollection.getLayers().filter(layer => this.targetLayerIds.includes(layer.layer.get("id")));
+                targetLayers = layerCollection.getLayers().filter(layer => this.targetLayerIds.includes(layer.layer.get("id")));
             }
-            return layerCollection.getLayers().filter(layer => layer.layer.get("typ") === "WFS");
+            else {
+                targetLayers = layerCollection.getLayers().filter(layer => layer.layer.get("typ") === "WFS");
+            }
+            return targetLayers.map(layer => {
+                return {
+                    id: layer.layer.get("id"),
+                    name: layer.layer.get("name")
+                };
+            });
         },
         LayersAvailable () {
             return this.filterLayers.length > 0 && this.targetLayers.length > 0;
@@ -46,16 +64,75 @@ export default {
         this.selectedTargetLayer = this.targetLayers[0] || null;
     },
     methods: {
-        applyFilter () {
+        async applyFilter () {
+            if (!this.selectedFilterLayer || !this.selectedTargetLayer) {
+                return;
+            }
             this.loading = true;
-            // Simulate request
-            setTimeout(() => {
+
+            const filterLayer = layerCollection.getLayerById(this.selectedFilterLayer.id),
+                targetLayer = layerCollection.getLayerById(this.selectedTargetLayer.id),
+                filterFeatures = filterLayer.layerSource.getFeatures(),
+                coordinates = filterFeatures.map(feature => feature.getGeometry()),
+
+                service = targetLayer.attributes.url,
+                featureType = targetLayer.attributes.featureType,
+                featureNS = targetLayer.attributes.featureNS,
+                featurePrefix = targetLayer.attributes.prefix,
+                geometryName = targetLayer.layerSource.getFeatures()[0].getGeometryName() || "geom",
+                epsg = "EPSG:25832"; // von map abgreifen
+
+            try {
+                const filters = coordinates.map(geometry => intersects(geometryName, geometry)),
+                    combinedFilter = or(...filters),
+
+                    wfsFormat = new WFS(),
+                    node = wfsFormat.writeGetFeature({
+                        srsName: epsg,
+                        featureNS: featureNS,
+                        featurePrefix: featurePrefix,
+                        featureTypes: [featureType],
+                        filter: combinedFilter
+                    }),
+                    serializer = new XMLSerializer(),
+                    body = serializer.serializeToString(node),
+
+                    response = await fetch(service, {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "text/xml"
+                        },
+                        body: body,
+                        credentials: targetLayer.attributes.isSecured ? "include" : "omit"
+                    });
+
+                let responseText = "",
+                    features = null;
+
+
+                if (!response.ok) {
+                    throw new Error(`Request failed with status ${response.status}`);
+                }
+
+                responseText = await response.text();
+
+                features = wfsFormat.readFeatures(responseText, {
+                    dataProjection: targetLayer.get("srsName") || "EPSG:4326",
+                    featureProjection: filterLayer.get("srsName") || "EPSG:4326"
+                });
+
+                // eslint-disable-next-line no-console
+                console.log("WFS Features:", features);
+            }
+            catch (error) {
+                console.error("Error during WFS request:", error);
+            }
+            finally {
                 this.loading = false;
-                console.warn("Filtering with layer:", this.selectedFilterLayer);
-                console.warn("Filtering layer:", this.selectedTargetLayer);
-            }, 1000);
+            }
         }
     }
+
 };
 </script>
 
@@ -90,7 +167,7 @@ export default {
                         :key="idx"
                         :value="layer"
                     >
-                        {{ layer.layer.get("name") }}
+                        {{ layer.name }}
                     </option>
                 </select>
                 <select
@@ -103,7 +180,7 @@ export default {
                         :key="idx"
                         :value="layer"
                     >
-                        {{ layer.layer.get("name") }}
+                        {{ layer.name }}
                     </option>
                 </select>
                 <FlatButton
