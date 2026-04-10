0f5868b6175f75fc512bf2fd809de85ab14742fd
bruehlca <carina.bruehl@dataport.de>
2026-04-10 15:09:01 +0200

    remove console.logs

--- exporter/utils/download.js
+++ exporter/utils/download.js
@@ -81,10 +81,6 @@ async function downloadVectorBaseLayer (vectorBaseLayer, format) {
         });
     let blob, gpkg, gpkgBytes;
 
-    console.log("[DEBUG] downloadVectorBaseLayer - features:", features);
-    console.log("[DEBUG] downloadVectorBaseLayer - featureProjection:", featureProjection);
-    console.log("[DEBUG] downloadVectorBaseLayer - geojson:", geojson);
-
     switch (format) {
         case "shp":
             // download as zipped shapefile will be triggered automatically by this function
